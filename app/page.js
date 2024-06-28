63139e227ecc57d5608e3c1a37d97a7c50a503bb
Samar Hayat <samarraja228@gmail.com>
2024-06-28 23:15:09 +0500

    11158

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 import BlogList from "@/components/BlogList";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 export default function Home() {
   return (
     <>
       <Header />
       <BlogList />
+      <Footer />
     </>
   );
 }

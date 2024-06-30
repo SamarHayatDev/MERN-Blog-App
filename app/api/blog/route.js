4e3a7c4bfeb19f2b86fa63a4844742001796a361
Samar Hayat <samarraja228@gmail.com>
2024-06-30 15:33:55 +0500

    20224

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,4 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
 import { writeFile } from "fs/promises";
 const { NextResponse } = require("next/server");
 
@@ -21,6 +22,16 @@ export async function POST(request) {
   const path = `./public/${timestamp}_${image.name}`;
   await writeFile(path, buffer);
   const imgUrl = `/${timestamp}_${image.name}`;
-  console.log(imgUrl);
-  return NextResponse.json({ imgUrl });
+
+  const blogData = {
+    title: `${formData.get("title")}`,
+    description: `${formData.get("description")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get("authorImg")}`,
+  };
+  await BlogModel.create(blogData);
+  console.log("blog saved");
+  return NextResponse.json({ success: true, msg: "blog added" });
 }

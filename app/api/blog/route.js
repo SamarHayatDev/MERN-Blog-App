69e2383df9d1b8c075a2b4ffaa797c8bfbe3e0ec
Samar Hayat <samarraja228@gmail.com>
2024-06-30 12:17:41 +0500

    15412 + error

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,6 +1,26 @@
+import { ConnectDB } from "@/lib/config/db";
+import { writeFile } from "fs/promises";
 const { NextResponse } = require("next/server");
 
+const LoadDB = async () => {
+  await ConnectDB();
+};
+LoadDB();
+
 export async function GET(request) {
-  console.log("Blog GET Hit");
   return NextResponse.json({ msg: "Api Working" });
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+  console.log(imgUrl);
+  return NextResponse.json({ imgUrl });
 }
